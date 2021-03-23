21e3f3d208bae9ac546933b48761520423079a43
Gildardo Mendez <vowser@outlook.com>
2021-03-23 11:32:21 -0600

    Including EJS to Project

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const ejs = require("ejs");
 
 const app = new express();
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true}));
 
 app.route("/")
 .get(function(req, res){
   console.log("--Route GET /");
-  res.send("Hello World");
+  let date = new Date().toLocaleDateString('en-US');
+
+  res.render("home", {date : date});
+})
+.post(function(req, res){
+  console.log("--Route POST /");
+  
+  res.send("Hi " + req.body.username);
 });
 app.listen(3000, function(){
   console.log("Server running on port 3000.");
